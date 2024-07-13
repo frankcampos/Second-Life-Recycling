cd9e0a064b457c37396368801a8fd720a77e4456
jmmyfam <pham_j@ymail.com>
2024-07-13 11:16:00 -0500

    registration form updated

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -6,22 +6,81 @@ import { registerUser } from '../utils/auth'; // Update with path to registerUse
 
 function RegisterForm({ user, updateUser }) {
   const [formData, setFormData] = useState({
-    bio: '',
+    first_name: '',
+    last_name: '',
+    photo: '',
+    email: '',
     uid: user.uid,
   });
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    registerUser(formData).then(() => updateUser(user.uid));
+    registerUser(formData)
+      .then(() => updateUser(user.uid))
+      .catch((error) => {
+        console.error('Registration error:', error);
+      });
   };
 
   return (
     <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Gamer Bio</Form.Label>
-        <Form.Control as="textarea" name="bio" required placeholder="Enter your Bio" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
-        <Form.Text className="text-muted">Let other gamers know a little bit about you...</Form.Text>
+      <Form.Group className="mb-3" controlId="formFirstName">
+        <Form.Label>First Name</Form.Label>
+        <Form.Control
+          type="text"
+          name="first_name"
+          placeholder="Enter your first name"
+          value={formData.first_name}
+          onChange={handleChange}
+          required
+        />
       </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formLastName">
+        <Form.Label>Last Name</Form.Label>
+        <Form.Control
+          type="text"
+          name="last_name"
+          placeholder="Enter your last name"
+          value={formData.last_name}
+          onChange={handleChange}
+          required
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formPhoto">
+        <Form.Label>Photo</Form.Label>
+        <Form.Control
+          type="text"
+          name="photo"
+          placeholder="Enter URL of your photo"
+          value={formData.photo}
+          onChange={handleChange}
+          required
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          type="email"
+          name="email"
+          placeholder="Enter your email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <Form.Text className="text-muted" />
+      </Form.Group>
+
       <Button variant="primary" type="submit">
         Submit
       </Button>
